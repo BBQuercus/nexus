84dbc48eef4a10aefa3000bfd92bc65809fc78bf
Bastian Eichenberger <bastian@eichenbergers.ch>
2026-03-26 22:29:24 +0100

    Use RAILWAY_GIT_COMMIT_SHA for build version tracking

--- backend/version.py
+++ backend/version.py
@@ -3,4 +3,4 @@
 import os
 
 VERSION = os.environ.get("RELEASE_VERSION", "0.1.0-dev")
-BUILD_SHA = os.environ.get("BUILD_SHA", "unknown")
+BUILD_SHA = os.environ.get("BUILD_SHA", os.environ.get("RAILWAY_GIT_COMMIT_SHA", "unknown"))
